ebb52fcef33943eb3ebd38e4266c2462943129af
Jerome Quelin <jquelin@gmail.com>
2022-10-07 15:33:38 +0200

    basic config pytrisk module

--- pytrisk/config.py
+++ pytrisk/config.py
@@ -15,6 +15,36 @@
 # along with pytrisk. If not, see <https://www.gnu.org/licenses/>.
 #
 
-def foo():
-    print('foo')
+from pathlib import Path
+import appdirs
+import contextlib
 
+# create config directory if needed
+_config_dir = Path(appdirs.user_config_dir('pytrisk'))
+_config_dir.mkdir(parents=True, exist_ok=True)
+
+# read & parse config file
+_config_file = Path(_config_dir, 'pytrisk.cfg')
+_config = {}
+
+with contextlib.suppress(FileNotFoundError):
+    with _config_file.open() as file:
+        for line in file.readlines():
+            line = line.rstrip()
+            (key, value) = line.split('=', 1)
+            _config[key] = value
+
+def get(key, default=None):
+    if key in _config:
+        return _config[key]
+    return default
+
+def set(key, value):
+    # store new value
+    _config[key] = value
+    # then write file for persistence
+    with _config_file.open('w') as file:
+        for key, value in _config.items():
+            line = f'{key}={value}\n'
+            print(line)
+            file.write(line)
