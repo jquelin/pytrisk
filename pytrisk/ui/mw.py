b2a8c107422bdc6caedf21390512e22924249920
Jerome Quelin <jquelin@gmail.com>
2026-04-15 15:05:30 +0200

    clean up mw imports

--- pytrisk/ui/mw.py
+++ pytrisk/ui/mw.py
@@ -15,28 +15,23 @@
 # along with pytrisk. If not, see <https://www.gnu.org/licenses/>.
 #
 
-from pytrisk import config  # should go first
 
-from pytrisk.locale import _
-from ..logger import log
-import pytrisk.data
-from pytrisk.ui.tkhelper import Action
 
 from pathlib import Path
-import PIL.Image
-import PIL.ImageTk
 import tkinter as tk
-from tkinter import *
 from tkinter import ttk
-import types
 
+#from pytrisk import config
+import pytrisk.data
+from pytrisk.locale import _
+from pytrisk.logger import log
 from pytrisk.constants import appinfo
 from pytrisk.ui.utils         import Icons
 from pytrisk.ui.views.menu    import MenuView
 from pytrisk.ui.views.toolbar import ToolbarView
 
 
-class MainWindow(Tk):
+class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
 
@@ -65,32 +60,32 @@ class MainWindow(Tk):
 
 
     def _build_startup_frame(self):
-        frame = Frame(self)
-        frame.pack(side=TOP, expand=True, fill=BOTH)
+        frame = tk.Frame(self)
+        frame.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
 
-        f = Frame(frame)
-        f.pack(side=LEFT, expand=True, fill=Y)
+        f = tk.Frame(frame)
+        f.pack(side=tk.LEFT, expand=True, fill=tk.Y)
 
-        lab = Label(f, text=_('Map'))
-        lab.pack(side=TOP)
-        lb_maps = Listbox(f)
-        lb_maps.pack(side=TOP, expand=True, fill=Y)
+        lab = tk.Label(f, text=_('Map'))
+        lab.pack(side=tk.TOP)
+        lb_maps = tk.Listbox(f)
+        lb_maps.pack(side=tk.TOP, expand=True, fill=tk.Y)
 
         maps = sorted(pytrisk.data.all_maps(), key=lambda x: x.title)
         for m in maps:
-            lb_maps.insert(END, m.title)
+            lb_maps.insert(tk.END, m.title)
 
-        f_players = Frame(frame)
-        f_players.pack(side=LEFT, expand=True, fill=BOTH)
-        lab = Label(f_players, text=_('Players'))
-        lab.pack(side=TOP)
-        f = Frame(f_players)
-        f.pack(side=TOP, fill=X)
-        lab = Label(f, text=_('Number of players'))
-        lab.pack(side=LEFT)
-        scale = Scale(f, orient=HORIZONTAL, from_=2, to=10)
+        f_players = tk.Frame(frame)
+        f_players.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        lab = tk.Label(f_players, text=_('Players'))
+        lab.pack(side=tk.TOP)
+        f = tk.Frame(f_players)
+        f.pack(side=tk.TOP, fill=tk.X)
+        lab = tk.Label(f, text=_('Number of players'))
+        lab.pack(side=tk.LEFT)
+        scale = tk.Scale(f, orient=tk.HORIZONTAL, from_=2, to=10)
         scale.set(3)
-        scale.pack(side=LEFT, fill=X)
+        scale.pack(side=tk.LEFT, fill=tk.X)
 
 
 
