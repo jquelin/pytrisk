73ed96c55c0bfe1856a4d25f3f0fcb9168364594
Jerome Quelin <jquelin@gmail.com>
2026-05-17 11:59:46 +0200

    populating players frame

--- pytrisk/ui/views/players.py
+++ pytrisk/ui/views/players.py
@@ -9,11 +9,26 @@ from pytrisk.logger    import log
 
 class PlayersView(tk.LabelFrame):
     def __init__(self, parent, controller, event_bus):
+        log.info('Creating players frame')
         super().__init__(parent, text=' ' + _('Players') + ' ')
 
         # Subscribe to events & store the controller for later use.
         event_bus.subscribe(self)
         self.controller = controller
 
-        a = tk.Label(self, text='a')
-        a.pack()
+        # Create ui
+        self._create_ui()
+
+
+    # -- Private methods
+
+    def _create_ui(self):
+        """Create the ui."""
+
+        subframe = tk.Frame(self,bg='yellow')
+        subframe.pack(side=tk.TOP, padx=5, pady=5, expand=True)
+
+        for p in self.controller.get_players():
+            f = tk.Frame(subframe, width=16, height=16, bg=p.color)
+            f.pack(side=tk.LEFT)
+
